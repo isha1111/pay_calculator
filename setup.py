758751e65c79256c9fd7a96e327387bd4a90be42
isha <isha.negi19@gmail.com>
2019-09-07 22:39:45 +1000

    cm

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
 
 requires = [
     'plaster_pastedeploy',
-    'setuptools == 41.0.1',
+    'setuptools == 39.0.1',
     'pyramid',
     'pyramid_mako',
     'pyramid_debugtoolbar',
